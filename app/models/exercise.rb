6701d1ea5c3648c2d50afa2bd692e5adf60a5c09
Anthony Ellis <ajellis1989@gmail.com>
2015-10-02 13:07:20 -0400

    Pushing progress-- need exercise_type model first

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,12 @@
 class Exercise < ActiveRecord::Base
+  # belongs_to: exercise_type
+
+  validates :exercise_type_id, presence: true
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :calories_burned, presence: true
+
+
+
+
 end
--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ExerciseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "New Exercise Can Be Saved" do
+    a = Exercise.create(name: "Jog Around the Park", calories_burned: "350", exercise_type_id: 1, date: Date.today)
+
+    assert a.save
+  end
+
+
 end
