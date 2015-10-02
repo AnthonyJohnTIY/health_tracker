ba0ebaf10a4a709ecb6ad6af49ed8a408c3bcef2
Anthony Ellis <ajellis1989@gmail.com>
2015-10-02 13:23:59 -0400

    Fixed error in exercises routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :exercises
   root 'dashboard#home'
 
   get '/home' => 'dashboard#home'
+
+  resources :exercises
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
