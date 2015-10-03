7c7161543cc41427e3ad7a6e9730cf59bce9addd
Anthony Ellis <ajellis1989@gmail.com>
2015-10-03 14:01:13 -0400

    Cleans up links

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -2,6 +2,10 @@ class Weight < ActiveRecord::Base
   validates :date, uniqueness: true
 
   def self.current_weight
-    self.all.sort {|a, b| a.date <=> b.date}.last.weight
+    if self.count == 0
+      "No Weights Entered"
+    else
+      self.all.sort {|a, b| a.date <=> b.date}.last.weight
+    end
   end
 end
